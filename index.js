c4c8fdb54b598695cc1ceee9a8d5d1d82f3aa89d
Horatiu Mitrea <horatiu.mitrea@gmail.com>
2021-03-06 22:20:01 -0500

    Fixing the api, images are not fetching, may need to change the data from them

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
 const url = "http://localhost:3000/cards";
-// For the image
-const image = document.createElement("img");
 
-let counter = 0;
-const increaseCounter = function () {
-  return counter++;
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => displayData(data));
+
+displayData = (data) => {
+  //   const dataElementDiv = document.querySelector("#cards");
+  const titleDiv = document.querySelector("#title");
+  const subtitleDiv = document.querySelector("#subtitle");
+  const textDiv = document.querySelector("#textDiv");
+
+  data.forEach((el) => {
+    console.log(el.title);
+    console.log(el.subtitle);
+    console.log(el.text);
+    console.log("-------------------------");
+
+    const titleEl = document.createElement("div");
+    const subtitleEl = document.createElement("div");
+    const textEl = document.createElement("div");
+
+    titleEl.innerHTML = el.title;
+    subtitleEl.innerHTML = el.subtitle;
+    textEl.innerHTML = el.text;
+
+    title.append(titleEl);
+    subtitle.append(subtitleEl);
+    text.append(textEl);
+  });
 };
+// Function to get the data, commented out and readgin this doc
+// https://jack72828383883.medium.com/how-to-use-javascript-fetch-to-display-api-results-in-html-7aa59936ed30
+// async function getData() {
+//   const response = await fetch(url);
+//   const data = await response.json();
+//   console.log("The array of data is", data);
 
-// Function to get the data
-async function getData() {
-  const response = await fetch(url);
-  const data = await response.json();
-  // Destrucutring our info
-  //   if (data.length === 9) {
-  //     increaseCounter();
-  //   }
+//   const { id, title, subtitle, text, image_url } = data[0];
 
-  //   let id = data[counter].id;
-  //   let title = data[counter].title;
-  //   let subtitle = data[counter].subtitle;
-  //   let text = data[counter].text;
-  //   let image_url = data[counter].image_url;
-  const { id, title, subtitle, text, image_url } = data[0];
+//   //   image.src = image_url;
+//   document.getElementById("title").textContent = title;
+//   document.getElementById("subtitle").textContent = subtitle;
+//   document.getElementById("text").textContent = text;
+// }
 
-  //   image.src = image_url;
-  document.getElementById("title").textContent = title;
-  document.getElementById("subtitle").textContent = subtitle;
-  document.getElementById("text").textContent = text;
-}
-
-getData();
+// getData();
