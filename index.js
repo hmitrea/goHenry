0d4e23a59e8445d679dea9da1a0a6f2be5cb943e
Horatiu Mitrea <horatiu.mitrea@gmail.com>
2021-03-06 21:07:01 -0500

    Making the API work better

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-console.log("hi");
-
 const url = "http://localhost:3000/cards";
+// For the image
+const image = document.createElement("img");
 
-// Fetching the URL data
-fetch(url)
-  .then((data) => {
-    return data.json();
-  })
-  .then((res) => {
-    console.log(res);
-  });
+let counter = 0;
+const increaseCounter = function () {
+  return counter++;
+};
+
+// Function to get the data
+async function getData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  // Destrucutring our info
+  //   if (data.length === 9) {
+  //     increaseCounter();
+  //   }
+
+  //   let id = data[counter].id;
+  //   let title = data[counter].title;
+  //   let subtitle = data[counter].subtitle;
+  //   let text = data[counter].text;
+  //   let image_url = data[counter].image_url;
+  const { id, title, subtitle, text, image_url } = data[0];
+
+  //   image.src = image_url;
+  document.getElementById("title").textContent = title;
+  document.getElementById("subtitle").textContent = subtitle;
+  document.getElementById("text").textContent = text;
+}
+
+getData();
