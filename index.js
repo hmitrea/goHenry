ecae8a0b852bdef885a0b707e66f55bee0c06fa2
Horatiu Mitrea <horatiu.mitrea@gmail.com>
2021-03-07 03:08:01 -0500

    working

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 // Selecting the IDs from the DOM
 const dataElementDiv = document.querySelector("#allCards");
-const idDiv = document.querySelector("#card");
+const idDiv = document.querySelector("card");
 
-const titleDiv = document.querySelector("#title");
-const subtitleDiv = document.querySelector("#subtitle");
-const textDiv = document.querySelector("#textDiv");
+const titleDiv = document.querySelector("title");
 
 // The API url which loads from a different port
 const url = "http://localhost:3000/cards";
@@ -25,8 +23,11 @@ displayData = (data) => {
     subtitleEl.innerHTML = el.subtitle;
     textEl.innerHTML = el.text;
 
-    document.getElementById("title").appendChild(titleEl);
-    document.getElementById("subtitle").appendChild(subtitleEl);
-    document.getElementById("text").appendChild(textEl);
+    document.getElementsByClassName("title")[0].append(titleEl);
+    document.getElementsByClassName("subtitle")[0].append(subtitleEl);
+    document.getElementsByClassName("text")[0].append(textEl);
+
+    // idDiv.appendChild(titleEl);
+    // document.getElementsByClassName(idDiv)[0].appendChild(titleEl);
   });
 };
