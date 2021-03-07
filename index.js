66ff8de98d9ff4474c5f73726ebb36a3b8d113f4
Horatiu Mitrea <horatiu.mitrea@gmail.com>
2021-03-07 02:35:14 -0500

    API better fix

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const url = "http://localhost:3000/cards";
+// Selecting the IDs from the DOM
+const dataElementDiv = document.querySelector("#allCards");
+const idDiv = document.querySelector("#card");
 
+const titleDiv = document.querySelector("#title");
+const subtitleDiv = document.querySelector("#subtitle");
+const textDiv = document.querySelector("#textDiv");
+
+// The API url which loads from a different port
+const url = "http://localhost:3000/cards";
 fetch(url)
   .then((res) => res.json())
+
+  // The function displayData  will create as many elements as needed
   .then((data) => displayData(data));
 
 displayData = (data) => {
-  //   const dataElementDiv = document.querySelector("#cards");
-  const titleDiv = document.querySelector("#title");
-  const subtitleDiv = document.querySelector("#subtitle");
-  const textDiv = document.querySelector("#textDiv");
-
+  //   console.log(data);
   data.forEach((el) => {
-    console.log(el.title);
-    console.log(el.subtitle);
-    console.log(el.text);
-    console.log("-------------------------");
-
     const titleEl = document.createElement("div");
     const subtitleEl = document.createElement("div");
     const textEl = document.createElement("div");
@@ -24,24 +25,8 @@ displayData = (data) => {
     subtitleEl.innerHTML = el.subtitle;
     textEl.innerHTML = el.text;
 
-    title.append(titleEl);
-    subtitle.append(subtitleEl);
-    text.append(textEl);
+    document.getElementById("title").appendChild(titleEl);
+    document.getElementById("subtitle").appendChild(subtitleEl);
+    document.getElementById("text").appendChild(textEl);
   });
 };
-// Function to get the data, commented out and readgin this doc
-// https://jack72828383883.medium.com/how-to-use-javascript-fetch-to-display-api-results-in-html-7aa59936ed30
-// async function getData() {
-//   const response = await fetch(url);
-//   const data = await response.json();
-//   console.log("The array of data is", data);
-
-//   const { id, title, subtitle, text, image_url } = data[0];
-
-//   //   image.src = image_url;
-//   document.getElementById("title").textContent = title;
-//   document.getElementById("subtitle").textContent = subtitle;
-//   document.getElementById("text").textContent = text;
-// }
-
-// getData();
